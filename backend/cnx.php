a30f2877d5c4cdc2e0e7690d259d947479eb1a2f
Lucas <m>
2023-10-14 15:50:42 +0200

    Création Backend

---
+++ backend/cnx.php
@@ -0,0 +1,10 @@
+<?php
+require_once('config.php');
+try {
+    $cnx = new PDO(PDO_DATABASE, DATABASE_USER, DATABASE_PASSWORD); 
+    echo "Connexion réussie"; 
+}
+catch (PDOException $e) {
+    echo "ERREUR : La connexion a échouée";
+}
+?>
