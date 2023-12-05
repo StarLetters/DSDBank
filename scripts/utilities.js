4af25900b6f0436722a353888e551cdf255f7761
Hyprra <houangkeo@gmail.com>
2023-12-05 23:47:49 +0100

    red

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,8 +1,19 @@
 function displayElements(elements, displayValue) {
-    for (let i = 0; i < elements.length; i++) {
-      elements[i].style.display = displayValue;
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.display = displayValue;
+  }
+}
+
+function addRedClassToRowIfNegative(tableRows) {
+  for (let i = 0; i < tableRows.length; i++) {
+    const row = tableRows[i];
+    const lastCellValue = parseFloat(row.lastElementChild.textContent);
+    if (lastCellValue < 0) {
+      row.classList.add("red");
+    } else if (tableRows[i].classList.contains("red")) {
+      tableRows[i].classList.remove("red");
     }
   }
+}
 
-
-export { displayElements };
+export { displayElements, addRedClassToRowIfNegative, updateIfChangingPage };
