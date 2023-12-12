6b4f0ad7048728eab0269042e54bc90d88172d13
Adrien Baffioni <baffioniadrien23@gmail.com>
2023-12-12 12:44:07 +0100

    Sans comentaires inutiles

--- scripts/dataTable.js
+++ scripts/dataTable.js
@@ -105,18 +105,15 @@ function createTableWithDetails(data) {
     let tableContainer = document.getElementById('table-container');
     tableContainer.innerHTML = '';
 
-    // Créer un tableau HTML
     let table = document.createElement('table');
 
-    // Créer l'en-tête du tableau
     let thead = document.createElement('thead');
     let headerRow = document.createElement('tr');
 
-    // Parcourir les clés de la première entrée pour créer les en-têtes de colonnes
     Object.keys(data[0]).forEach(key => {
         if (!/^\d+$/.test(key)) {
             let th = document.createElement('th');
-            th.textContent = key.toString(); // Convertir la clé en chaîne de caractères
+            th.textContent = key.toString();
             headerRow.appendChild(th);
         }
     });
@@ -124,26 +121,24 @@ function createTableWithDetails(data) {
     thead.appendChild(headerRow);
     table.appendChild(thead);
 
-    // Créer le corps du tableau
     let tbody = document.createElement('tbody');
 
-    // Parcourir les données JSON et créer les lignes du tableau
     data.forEach(item => {
         let row = document.createElement('tr');
 
-        // Parcourir les valeurs de chaque entrée pour créer les cellules de données
         Object.entries(item).forEach(([key, value]) => {
             if (!/^\d+$/.test(key)) {
                 let cell = document.createElement('td');
                 cell.textContent = value;
-                cell.setAttribute('data-label', key); // Ajouter l'attribut data-th pour la correspondance avec les en-têtes de colonnes
+                cell.setAttribute('data-label', key);
                 if (key === 'N° Remise') {
-                    row.setAttribute('id', value);
+                    row.setAttribute('id', value); // Ajouter l'attribut id pour la correspondance avec les détails de la remise
                 }
                 row.appendChild(cell);
             }
         });
 
+        // Créer le tableau des détails de la remise sur un pop up
         row.addEventListener("click", async () => {
             nRemise = row.getAttribute('id');
             document.getElementById('details').style.display = 'block';
@@ -151,56 +146,9 @@ function createTableWithDetails(data) {
         });
 
         tbody.appendChild(row);
-        /*if (details && row.getAttribute('id') === nRemise) {
-            let secondRow = document.createElement('tr');
-            let tableCell = document.createElement('td');
-            tableCell.setAttribute('colspan', '7');
-            // Créer un tableau HTML détaillé
-            let detailedTable = document.createElement('table', 'detailed-table');
-
-            // Créer l'en-tête du tableau détaillé
-            let detailedThead = document.createElement('thead');
-            let detailedHeaderRow = document.createElement('tr');
-
-            Object.keys(detailedData[0]).forEach(key => {
-                if (!/^\d+$/.test(key)) {
-                    let th = document.createElement('th');
-                    th.textContent = key.toString(); // Convertir la clé en chaîne de caractères
-                    detailedHeaderRow.appendChild(th);
-                }
-            });
-            detailedThead.appendChild(detailedHeaderRow);
-            detailedTable.appendChild(detailedThead);
-
-            // Créer le corps du tableau détaillé
-            let detailedTbody = document.createElement('tbody');
-
-            // Parcourir les données JSON et créer les lignes du tableau détaillé
-            detailedData.forEach(detailedItem => {
-                let detailedRow = document.createElement('tr');
-                
-                Object.entries(detailedItem).forEach(([key, value]) => {
-                    if (!/^\d+$/.test(key)) {
-                        let cell = document.createElement('td');
-                        cell.textContent = value;
-                        cell.setAttribute('data-label', key); // Ajouter l'attribut data-th pour la correspondance avec les en-têtes de colonnes
-                        detailedRow.appendChild(cell);
-                    }
-                });
-                
-                detailedTbody.appendChild(detailedRow);
-            });
-            detailedTable.appendChild(detailedTbody);
-            tableCell.appendChild(detailedTable);
-            secondRow.appendChild();
-            tbody.appendChild(secondRow);
-        }*/
     });
 
     table.appendChild(tbody);
-
-    // Ajouter le tableau au document
-
     tableContainer.appendChild(table);
 }
 
