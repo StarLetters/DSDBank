e0fc5dd35387c0a6e11311a5a50a948e4d2e9fb3
Elias_lhl <eliaslahlouh@gmail.com>
2023-12-16 01:13:36 +0100

    remises

--- pages/remises.php
+++ pages/remises.php
@@ -48,74 +48,72 @@ setcookie('cnxToken', $_SESSION['cnxToken'], [
                     </div>
                 </div>
                 <div class="row mt-4 ct1 flex-column flex-md-row mb-3">
-                    <div class="hidePO col-12 col-md-6 align-items-center flex-row order-md-2">
-                        <div class="col-12 mb-3 mb-md-0" id="selectDate">
-                            <div class="row">
-                                <div class="col-6 col-md-6">
-                                    <label for="startDate">Date de début :</label>
-                                    <input type="date" id="startDate" class="form-control form-control-sm date col-12">
-                                </div>
-                                <div class="col-6 col-md-6">
-                                    <label for="endDate">Date de fin :</label>
-                                    <input type="date" id="endDate" class="form-control form-control-sm date col-12">
-                                </div>
-                            </div>
-                        </div>
+                <div class="hidePO col-12 col-md-6 align-items-center flex-row order-md-2">
+    <div class="col-12 mb-3 mb-md-0" id="selectDate">
+        <div class="row clm">
+            <div class="col-6 col-md-6">
+                <label for="startDate">Date de début :</label>
+                <input type="date" id="startDate" class="form-control form-control-sm date col-12">
+            </div>
+            <div class="col-6 col-md-6">
+                <label for="endDate">Date de fin :</label>
+                <input type="date" id="endDate" class="form-control form-control-sm date col-12">
+            </div>
+        </div>
+    </div>
 
-                        <?php
-                        if ($role == 1) {
-                            echo "
-                        <div class=\"row mb-5\">
-                        <div class=\"col-12 col-md-4 offset-md-4\">
-                    <input type=\"text\" id=\"nSiren\" class=\"form-control form-control-sm date\" placeholder=\"N°SIREN\">
-                    </div>
-                    </div>
-                    <div class=\"row mb-5\">
-                    <div class=\"col-12 col-md-4 offset-md-4\">
-                        <input type=\"text\" id=\"raison\" class=\"form-control form-control-sm date\" placeholder=\"Raison Sociale\">
-                    </div>
-                    </div>";
-                        }
-                        echo "  
-                    <div id=\"email\" class=\"hidden\">" . $_SESSION['email'] . "</div>
-                    ";
-                        ?>
-                    </div>
+    <?php
+    if ($role == 1) {
+        echo '
+        <div class="row mb-5 clm">
+            <div class="col-6 col-md-6">
+                <label for="nSiren">N°SIREN</label>
+                <input type="text" id="nSiren" class="form-control form-control-sm date col-12">
+            </div>
+            <div class="col-6 col-md-6">
+                <label for="raison">Raison Sociale</label>
+                <input type="text" id="raison" class="form-control form-control-sm date col-12">
+            </div>
+        </div>';
+    }
+    echo '<div id="email" class="hidden">' . $_SESSION['email'] . '</div>';
+    ?>
+</div>
                     <div class="col-12 col-md-6" id="search-container">
                         <label class="mt-2" for="nRemise">N°Remise</label>
                         <input type="text" id="nRemise" class="form-control form-control-sm date"
                             placeholder="N°Remise">
 
-                    <div class="d-flex flex-column flex-md-row mt-2">
-                        <button id="resetButton" class="mx-1">Effacer</button>
-                        <button id="searchButton" class="mx-1">Rechercher</button>
-                    </div>
+                        <div class="d-flex flex-column flex-md-row mt-2">
+                            <button id="resetButton" class="mx-1">Effacer</button>
+                            <button id="searchButton" class="mx-1">Rechercher</button>
+                        </div>
                     </div>
                 </div>
             </div>
 
 
-<div class="row justify-content-between align-items-start mx-5 mb-3">
-    <div id="items-per-page-container">
-        <label for="items-per-page">Éléments par page:</label>
-        <select id="items-per-page" class="item-selecteur">
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-        </select>
-    </div>
-    </div>
-    <div class="d-flex flex-row clm">
-        <div id="order-by-container" class="col-md-12 mb-2 mx-4 col-lg-6 order-by">
-            <label for="order-by">Trier par:</label>
-            <select id="order-by" class="item-selecteur col-6 col-sm-6">
-                <option value="montantDesc">Montant décroissant</option>
-                <option value="montantAsc">Montant croissant</option>
-            </select>
-        </div>
-        <div id="results-container" class="text-right col-5"></div>
+            <div class="row justify-content-between align-items-start mx-5 mb-3">
+                <div id="items-per-page-container">
+                    <label for="items-per-page">Éléments par page:</label>
+                    <select id="items-per-page" class="item-selecteur">
+                        <option value="3">3</option>
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                    </select>
+                </div>
+            </div>
+            <div class="d-flex flex-row clm">
+                <div id="order-by-container" class="col-md-12 mb-2 mx-4 col-lg-6 order-by">
+                    <label for="order-by">Trier par:</label>
+                    <select id="order-by" class="item-selecteur col-6 col-sm-6">
+                        <option value="montantDesc">Montant décroissant</option>
+                        <option value="montantAsc">Montant croissant</option>
+                    </select>
+                </div>
+                <div id="results-container" class="text-right col-5"></div>
 
-</div>
+            </div>
             <div class="row">
                 <div id="table-container"></div>
             </div>
