d034ea96a72e68905b8016e15a2da4780e1b7266
Lucas <116306734+Luucas7@users.noreply.github.com>
2023-12-18 01:07:09 +0100

    Fix des graphiques, basent sur le CA

--- pages/home.php
+++ pages/home.php
@@ -35,12 +35,13 @@ include('../backend/cnx.php');
 </head>
 
 <!-- <body style="background-color: black;"> -->
-    <body>
+
+<body>
     <div class="">
         <div id="wrapper">
             <?php include('../includes/header.php'); ?>
             <div class="color-background"></div>
-            <div class="blurred-background"></div> 
+            <div class="blurred-background"></div>
             <div id="content" class="content-show">
                 <section id="content-wrapper" class="no-margin-bottom d-flex justify-content-center text-center" style="top:0;position:inherit; height:100vh">
                     <div class="row-1">
@@ -81,49 +82,49 @@ include('../backend/cnx.php');
                 <div class="container text-center mt-5">
                     <div class="row">
                         <div class="col-md-8 offset-md-2">
-                        <div id="customerReviewsCarousel" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <!-- Avis client 1 -->
-        <div class="carousel-item active">
-            <img src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg" class="img-fluid rounded-circle" class="d-block mx-auto" alt="Client 1">
-            <h3 class="text-white">John Doe</h3>
-            <p class="text-white">Excellent service ! Je suis très satisfait de mon expérience avec cette entreprise. Le personnel était amical et compétent. Je recommande vivement leurs services.</p>
-        </div>
+                            <div id="customerReviewsCarousel" class="carousel slide" data-ride="carousel">
+                                <div class="carousel-inner">
+                                    <!-- Avis client 1 -->
+                                    <div class="carousel-item active">
+                                        <img src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg" class="img-fluid rounded-circle" class="d-block mx-auto" alt="Client 1">
+                                        <h3 class="text-white">John Doe</h3>
+                                        <p class="text-white">Excellent service ! Je suis très satisfait de mon expérience avec cette entreprise. Le personnel était amical et compétent. Je recommande vivement leurs services.</p>
+                                    </div>
 
-        <!-- Avis client 2 -->
-        <div class="carousel-item">
-            <img src="https://cdn-icons-png.flaticon.com/512/20/20863.png" class="img-fluid rounded-circle" class="d-block mx-auto" alt="Client 2">
-            <h3 class="text-white">Jane Smith</h3>
-            <p class="text-white">J'ai été impressionné par le professionnalisme de cette entreprise. Ils ont résolu mon problème rapidement et efficacement. Je les recommande sans hésitation.</p>
-        </div>
+                                    <!-- Avis client 2 -->
+                                    <div class="carousel-item">
+                                        <img src="https://cdn-icons-png.flaticon.com/512/20/20863.png" class="img-fluid rounded-circle" class="d-block mx-auto" alt="Client 2">
+                                        <h3 class="text-white">Jane Smith</h3>
+                                        <p class="text-white">J'ai été impressionné par le professionnalisme de cette entreprise. Ils ont résolu mon problème rapidement et efficacement. Je les recommande sans hésitation.</p>
+                                    </div>
 
-        <!-- Avis client 3 -->
-        <div class="carousel-item">
-            <img src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg" class="img-fluid rounded-circle" class="d-block mx-auto" alt="Client 3">
-            <h3 class="text-white">Michael Johnson</h3>
-            <p class="text-white">Le service client de cette entreprise est exceptionnel. Ils ont répondu à toutes mes questions et préoccupations de manière amicale et professionnelle. Je suis très satisfait de leur travail.</p>
-        </div>
+                                    <!-- Avis client 3 -->
+                                    <div class="carousel-item">
+                                        <img src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg" class="img-fluid rounded-circle" class="d-block mx-auto" alt="Client 3">
+                                        <h3 class="text-white">Michael Johnson</h3>
+                                        <p class="text-white">Le service client de cette entreprise est exceptionnel. Ils ont répondu à toutes mes questions et préoccupations de manière amicale et professionnelle. Je suis très satisfait de leur travail.</p>
+                                    </div>
 
-        <!-- Ajoutez ici d'autres avis clients -->
+                                    <!-- Ajoutez ici d'autres avis clients -->
 
-    </div>
-</div>
-
-                                <!-- Contrôles de navigation -->
-                                <a class="carousel-control-prev" href="#clientCarousel" role="button" data-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="sr-only">Précédent</span>
-                                </a>
-                                <a class="carousel-control-next" href="#clientCarousel" role="button" data-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="sr-only">Suivant</span>
-                                </a>
+                                </div>
                             </div>
+
+                            <!-- Contrôles de navigation -->
+                            <a class="carousel-control-prev" href="#clientCarousel" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Précédent</span>
+                            </a>
+                            <a class="carousel-control-next" href="#clientCarousel" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Suivant</span>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
--- scripts/unpaid.js
+++ scripts/unpaid.js
@@ -150,7 +150,6 @@ function initializeImp() {
 
   addListener();
   displayOrderBy();
-
   search();
 }
 
